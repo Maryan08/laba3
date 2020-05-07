f850f336b391ee6f9b19d2874644b7f944dd03bd
Maryan08 <mar08khymych@gmail.com>
2020-05-07 21:52:21 +0300

    first

---
+++ OOP Lab 3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP_Lab_3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть рядок");
+            string s = Console.ReadLine();
+            char[] c = s.ToCharArray();
+            int n = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (c[i] == '.' || c[i] == ',' || c[i] == ';' || c[i] == ':' || c[i] == '—' || c[i] == '!' || c[i] == '?' || c[i] == '"')
+                {
+                    n++;
+                }
+            }
+            Console.WriteLine("Кількість розділових знаків: {0}", n);
+            string[] s1 = s.Split();
+            char[] charsToTrim = { '.', ',', ';', ':', '—', '?', '!', '"' };
+            for (int i = 0; i < s1.Length; i++)
+                
+                {
+                    string s2 = s1[i].Trim(charsToTrim);
+                    char[] t = s2.ToCharArray();
+                    if (t.Length % 2 == 0)
+                    {
+                    
+                        Console.Write(s2 + " ");
+                    }
+                }
+        }
+    }
+}
